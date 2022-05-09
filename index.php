3f1f29aa464cb86fdcbbc5d330af2c0a6d9457de
Jehovanie <jehovanieram@gmail.com>
2022-05-09 10:06:06 +0300

    push all

--- index.php
+++ index.php
@@ -5,12 +5,6 @@ include_once("./_config/db.php");
 include_once("./_fonctions/fonction.php");
 
 
-include_once("./_classes/Articles.php");
-include_once("./_classes/Authors.php");
-include_once("./_classes/Categories.php");
-
-
-
 if (isset($_GET['page']) && !empty($_GET['page'])) {
 
     $page = trim(strtolower($_GET['page']));
@@ -20,15 +14,13 @@ if (isset($_GET['page']) && !empty($_GET['page'])) {
 
 $allpages = scandir('controllers/'); // scan all element.
 
+
+
 if (in_array($page . '_controller.php', $allpages)) {
 
-    include_once('controllers/' . $page . '_controller.php');
     include_once('modeles/' . $page . '_modele.php');
+    include_once('controllers/' . $page . '_controller.php');
     include_once('views/' . $page . '_view.php');
 } else {
     echo "Error : 404 ";
 }
-
-
-// var_dump($_SESSION);
-// die();
--- modeles/home_modele.php
+++ modeles/home_modele.php
@@ -3,4 +3,5 @@
   * On déclare les class qui est necessaire  dans chaque model.
   * On crées les class dans _class pour bien organise.
   */
-include_once("_classes/Articles.php");
+include_once '_classes/Articles.php';
+include_once '_classes/Categories.php';
